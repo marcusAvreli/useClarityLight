47ffb530bd8a89aae62b2bf1d933c276a497a369
marcusAvreli <markgav19@gmail.com>
2023-10-10 10:31:05 +0300

    initial_commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HTTP_INTERCEPTORS,HttpClientModule} from '@angular/common/http';
+import {HttpModule} from '@angular/http';
+import {reducer} from './common/reducers/index';
+import {CsvService} from './store/csvService';
+import {Effects} from './store/effects';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreModule} from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 
@@ -9,8 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+	,HttpModule
+	,HttpClientModule
+	,EffectsModule.run(Effects)
+	,StoreModule.provideStore(reducer)
   ],
-  providers: [],
+  providers: [CsvService,HttpModule,HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
